1d65305ed14759d56e84f23773df94ed9ec6c4f9
Fernando Diaz <fernandodiasfd780518@gmail.com>
2025-11-10 07:32:54 -0300

    Adiciona programa main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,37 @@
 #include <iostream> 
 
+void IntArray:: add(int i){
+
+
+
+int * val= new int[size()+1];
+
+for (size_t i=0; i<size(); i)
+tup[i]= get(i);
+}
+delete[]a;
+a=tup;
+++k;
 int Array::intarray() {
    delete[]a;
 }
-int Array::IntArray(int i)
-a(new int[1]), k(1)
+intArray::next(size_t id) const;
 {
 
-
-
+}
+return a[id];
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 struct IntArray {
 void add(int i);
 int get(size_t id)const;
 size_t size() const;
 size last()const;
+IntArray(int i);
+~ĨntArray();
+int*a;
+size_t k;
 }
 
 int main()
